32213cbd4ac7b3f2f6af734af4e640ed8994456d
Corey Light <coreylight@gmail.com>
2018-02-19 14:34:57 -0600

    Pass required plugins to export

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('@iopipe/trace');
-require('@iopipe/event-info');
+const tracePlugin = require('@iopipe/trace');
+const eventInfoPlugin = require('@iopipe/event-info');
 
 module.exports = {
-  plugins: ['@iopipe/trace', '@iopipe/event-info']
+  plugins: [tracePlugin(), eventInfoPlugin()]
 };
